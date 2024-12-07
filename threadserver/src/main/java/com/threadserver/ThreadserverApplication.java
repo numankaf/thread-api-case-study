5b70355ee4b24a0500685fc7926edfc1c463988c
Numan Kafadar <numankafadar97@gmail.com>
2024-12-07 16:46:14 +0300

    initial: server initialization

---
+++ threadserver/src/main/java/com/threadserver/ThreadserverApplication.java
@@ -0,0 +1,13 @@
+package com.threadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadserverApplication.class, args);
+	}
+
+}
---
+++ threadserver/src/test/java/com/threadserver/ThreadserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
