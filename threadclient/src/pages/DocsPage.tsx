d46b37b06ef8860de8979fec4f5636c54a86cac4
Numan Kafadar <numankafadar97@gmail.com>
2024-12-10 23:00:19 +0300

    docs: swagger-ui added to documentation in client

--- threadclient/src/pages/DocsPage.tsx
+++ threadclient/src/pages/DocsPage.tsx
@@ -1,5 +1,13 @@
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
 const DocsPage = () => {
-  return <div>DocsPage</div>;
+  return (
+    <div className="card">
+      <SwaggerUI supportedSubmitMethods={[]} url={`${baseURL}v3/api-docs`} />
+    </div>
+  );
 };
 
 export default DocsPage;
